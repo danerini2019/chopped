da422d0eb230170f51b3d3615c9ba75d8c56016a
danerini2019 <danerini@gmail.com>
2021-08-29 00:18:25 -0700

    commit

--- chopped.py
+++ chopped.py
@@ -10,12 +10,14 @@ read = pd.read_csv('data/archive/chopped.csv')
 df = pd.DataFrame(read)
 df['dessert'].replace('', np.nan, inplace=True)
 df.dropna(subset=['dessert'], inplace=True)
+
 # print(df['dessert'].iloc[555])
 
 # Function that accumulates ingredients and adds episode IDs for each row
 def get_ingredients(meal):
     episodes = []
     ingredients = []
+    meal_list = []
     for i in range(len(df[meal])):
         # print(i)
         # print(type(df[meal].iloc[i]))
@@ -23,21 +25,30 @@ def get_ingredients(meal):
         episode_id = int(str(df['season'].iloc[i]) + str(df['season_episode'].iloc[i]) + str(df['series_episode'].iloc[i]))
         for j in range(len(item_list)):
             episodes.append(episode_id)
-            ingredients.append(item_list[j])
+            meal_list.append(meal)
+            ingredients.append(item_list[j].strip())
     df_ingredients = pd.DataFrame({'episode_id':episodes, 'ingredients':ingredients})
     return df_ingredients
+
+# Function to add columns to ingredients
+def add_item_data(dataframe):
+    dataframe['count']
+
 
 # Function accumulates ingredients but cannot add episode IDs
 def do_not_use(dataframe, meal):
     item_list = df[meal].str.split(',', -1)
-    dataframe['ingredient'] = item_list.apply(pd.Series).stack().reset_index(drop = True)
+    dataframe['ingredients'] = item_list.apply(pd.Series).stack().reset_index(drop = True)
         
 app_items = get_ingredients('appetizer')
 ent_items = get_ingredients('entree')
 des_items = get_ingredients('dessert')
-all_basket_items = app_items.append(ent_items.append(des_items))
+all = app_items.append(ent_items.append(des_items))
+counts = all.value_counts().to_frame()
 
-print(len(all_basket_items))
-print(len(app_items))
-print(len(ent_items))
-print(len(des_items))
+print(counts.sort_v)
+
+# freq = all_basket_items['ingredients'].value_counts(bins=4)
+# print(freq)
+
+# print(all[all['ingredients'] == 'mustard greens'])
