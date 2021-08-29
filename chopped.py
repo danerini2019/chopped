316eca264d3627b162e53654e082f5d899fdbe91
danerini2019 <danerini@gmail.com>
2021-08-28 21:55:33 -0700

    meal ingredient list dataframes added

--- chopped.py
+++ chopped.py
@@ -8,52 +8,33 @@ pd.options.display.max_colwidth = None
 
 read = pd.read_csv('data/archive/chopped.csv')
 df = pd.DataFrame(read)
-df_ing = pd.DataFrame()
-
-# df[['app_ing_1','app_ing_2','app_ing_3','app_ing_4','app_ing_5','app_ing_6']] = df['appetizer'].str.split(',', -1)
-# df[['ent_ing_1','ent_ing_2','ent_ing_3','ent_ing_4','ent_ing_5','ent_ing_6']] = df['entree'].str.split(',', -1)
-# df[['des_ing_1','des_ing_2','des_ing_3']] = df['dessert'].str.split(',', -1)
-
-df['app_ing_type'] = type(df['appetizer'])
-
-# df['app_ing'] = df['appetizer'].str.split(',', -1).apply(len)
-# df['ent_ing'] = df['entree'].str.split(',', -1).apply(len)
-# # df['des_ing_count'] = df['dessert'].str.count(',').apply(int) + 1
-# # df['des_ing_count'] = int(df['des_ing_count'])
-# df['des_ing'] = df['dessert'].str.split(',', -1).fillna(0).apply(len)
 
 appetizer_items = pd.DataFrame()
 entree_items = pd.DataFrame()
 dessert_items = pd.DataFrame()
 
-
+# Function that accumulates ingredients and adds episode IDs for each row
 def episode(meal):
-    df_ing['episode_id'] = np.nan
-    df_ing['ingredient'] = ''
+    episodes = []
+    ingredients = []
     for i in range(len(df[meal])):
-        item_list = df[meal].str.split(',', -1)
-        episode_id = df['season'].iloc[i] + df['season_episode'].iloc[i] + df['series_episode'].iloc[i]
-        episode_break_point = len(df_ing['ingredient'])
-        print(episode_break_point)
+        item_list = df[meal].iloc[i].split(',', -1)
+        episode_id = int(str(df['season'].iloc[i]) + str(df['season_episode'].iloc[i]) + str(df['series_episode'].iloc[i]))
         for j in range(len(item_list)):
-            df_ing['episode_id'] = episode_id
-        
-    df_ing['ingredient'] =  item_list.apply(pd.Series).stack().reset_index(drop = True)
-        
+            episodes.append(episode_id)
+            ingredients.append(item_list[j])
+    df_ingredients = pd.DataFrame({'episode_id':episodes, 'ingredients':ingredients})
+    return df_ingredients
 
+# Function accumulates ingredients but cannot add episode IDs
 def accumulate_ingredients(dataframe, meal):
     item_list = df[meal].str.split(',', -1)
     dataframe['ingredient'] = item_list.apply(pd.Series).stack().reset_index(drop = True)
         
+app_items = episode('appetizer')
+ent_items = episode('entree')
+des_items = episode('dessert')
 
-episode('appetizer')
-episode('entree')
-episode('dessert')
-# item_list = df['appetizer'].str.split(',', -1)
-# df_ing['app_ing'] = item_list.apply(pd.Series).stack().reset_index(drop = True)
-
-print(df_ing.head())
-# print(df['num_ing'].apply(len))
-# print(df.app_ing_count.unique())
-# print(df.ent_ing_count.unique())
-# print(df.des_ing_count.unique())
+print(app_items.head(20))
+print(ent_items.head(20))
+print(des_items.head(20))
