0bb132adf03c63a5f86cd6ff4df8e2d0f3fab887
danerini2019 <danerini@gmail.com>
2021-08-28 22:54:33 -0700

    drop null

--- chopped.py
+++ chopped.py
@@ -8,12 +8,17 @@ pd.options.display.max_colwidth = None
 
 read = pd.read_csv('data/archive/chopped.csv')
 df = pd.DataFrame(read)
+df['dessert'].replace('', np.nan, inplace=True)
+df.dropna(subset=['dessert'], inplace=True)
+# print(df['dessert'].iloc[555])
 
 # Function that accumulates ingredients and adds episode IDs for each row
-def accumulate_ingredients(meal):
+def get_ingredients(meal):
     episodes = []
     ingredients = []
     for i in range(len(df[meal])):
+        # print(i)
+        # print(type(df[meal].iloc[i]))
         item_list = df[meal].iloc[i].split(',', -1)
         episode_id = int(str(df['season'].iloc[i]) + str(df['season_episode'].iloc[i]) + str(df['series_episode'].iloc[i]))
         for j in range(len(item_list)):
@@ -27,10 +32,12 @@ def do_not_use(dataframe, meal):
     item_list = df[meal].str.split(',', -1)
     dataframe['ingredient'] = item_list.apply(pd.Series).stack().reset_index(drop = True)
         
-app_items = accumulate_ingredients('appetizer')
-ent_items = accumulate_ingredients('entree')
-des_items = accumulate_ingredients('dessert')
+app_items = get_ingredients('appetizer')
+ent_items = get_ingredients('entree')
+des_items = get_ingredients('dessert')
+all_basket_items = app_items.append(ent_items.append(des_items))
 
-print(app_items.head(20))
-print(ent_items.head(20))
-print(des_items.head(20))
+print(len(all_basket_items))
+print(len(app_items))
+print(len(ent_items))
+print(len(des_items))
