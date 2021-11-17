804cb04d93c9a794ff5554972acee24c8d6afe9a
danerini2019 <danerini@gmail.com>
2021-11-16 22:03:06 -0800

    commit

---
+++ data_preview.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import csv
+
+# openfoodata = pd.read_csv('data/fr.openfoodfacts.org.products.csv', error_bad_lines=False, engine = 'python', lineterminator='\n', header=None)
+# df = pd.DataFrame(openfoodata)
+
+df = pd.read_csv('data/fr.openfoodfacts.org.products.csv', header=None   )
+
+df = df[0].str.split('\s\|\s', expand=True)
+
+# with open("sample.csv", 'rb') as file_obj:
+#     reader = csv.reader(file_obj)
+#     line_no = 1
+#     try:
+#         for row in reader:
+#             line_no += 1
+#     except Exception as e:
+#         print (("Error in the line number %d: %s %s" % (line_no, str(type(e)), e.message)))
+
+print(df.iloc[0:2])
